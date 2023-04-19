a0de2c97cf662cc131bc7b2f02329937ab5d2535
marc <marc-felix.schmitz@protonmail.com>
2023-04-19 16:14:57 +0200

    .

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.marc = import ./home.nix;
+            home-manager.users.marc = import ./home_manager/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
