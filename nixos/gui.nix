a33ee5f0b18fc3226af12e4069f1948952ff623e
marc <marc-felix.schmitz@protonmail.com>
2023-04-19 19:53:25 +0200

    switching to newest with gnome

--- nixos/gui.nix
+++ nixos/gui.nix
@@ -15,13 +15,13 @@
   # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Enable the BSPWM Desktop Environment
-  services.xserver.windowManager.bspwm.enable = true;
-  services.xserver.displayManager.defaultSession = "none+bspwm";
-  services.xserver.windowManager.bspwm.configFile = "/home/marc/.config/dotfiles-nixos/bspwmrc";
-  services.xserver.windowManager.bspwm.sxhkd.configFile = "/home/marc/.config/dotfiles-nixos/sxhkdrc";
+  #services.xserver.windowManager.bspwm.enable = true;
+  #services.xserver.displayManager.defaultSession = "none+bspwm";
+  #services.xserver.windowManager.bspwm.configFile = "/home/marc/.config/dotfiles-nixos/bspwmrc";
+  #services.xserver.windowManager.bspwm.sxhkd.configFile = "/home/marc/.config/dotfiles-nixos/sxhkdrc";
 
 }
