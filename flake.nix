d9f0b4561e49c151a0bd4ec60182fc4fa7485fad
marc <marc-felix.schmitz@protonmail.com>
2023-04-19 17:50:23 +0200

    penis

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    #hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
   outputs = {self, nixpkgs, ...}: {
