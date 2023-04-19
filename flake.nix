b34a6f1703b8e47725403f40c50f8945c37512be
marc <marc-felix.schmitz@protonmail.com>
2023-04-19 16:04:12 +0200

    penmis

--- flake.nix
+++ flake.nix
@@ -3,16 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
     hyprland.url = "github:hyprwm/Hyprland";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, hyprland, ... }: {
+  outputs = inputs@{ self, nixpkgs, hyprland, home-manager, ... }: {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         modules = [ 
-          hyprland.nixosModules.default 
-          { programs.hyprland.enable = true; }
+          #hyprland.nixosModules.default 
+          #{ programs.hyprland.enable = true; }
           ./nixos/configuration.nix
+           home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.marc = import ./home-manager/home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
         ];
       };
     };
--- nixos/gui.nix
+++ nixos/gui.nix
@@ -19,8 +19,8 @@
   # services.xserver.desktopManager.gnome.enable = true;
 
   # Enable the BSPWM Desktop Environment
-  #services.xserver.windowManager.bspwm.enable = true;
-  #services.xserver.displayManager.defaultSession = "none+bspwm";
-  #services.xserver.windowManager.bspwm.configFile = "/home/marc/.config/dotfiles-nixos/bspwmrc";
-  #services.xserver.windowManager.bspwm.sxhkd.configFile = "/home/marc/.config/dotfiles-nixos/sxhkdrc";
+  services.xserver.windowManager.bspwm.enable = true;
+  services.xserver.displayManager.defaultSession = "none+bspwm";
+  services.xserver.windowManager.bspwm.configFile = "/home/marc/.config/dotfiles-nixos/bspwmrc";
+  services.xserver.windowManager.bspwm.sxhkd.configFile = "/home/marc/.config/dotfiles-nixos/sxhkdrc";
 }
